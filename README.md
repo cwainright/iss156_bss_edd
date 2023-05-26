39639d9300b8c1b2c806ddd41151d24699761a2d
cwainright-nps <charles_wainright@nps.gov>
2023-05-26 13:06:05 -0400

    project configuration

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# iss156_bss_edd" 
+# Generate one EDD-formatted xlsx from many NCRN and collaborator data files
+
+Biological Stream Survey (BSS) data include monitoring and/or inventory data of fish, benthic macroinvertebrates, and abiotic habitat/water chemistry metrics.
+
+This repo combines all NPS NCRN BSS data into one data object and writes that object out as an excel file.
+
+# How to use this repo:
+1. Clone the repo from github to a new RStudio project (file -> new project -> version control -> paste the github url)
+2. Copy the "data/NCRN_MBSS_be_2022.mdb" from DOI sharepoint to your project's `data/ncrn` directory
+    - https://doimspp.sharepoint.com/:u:/r/sites/NCRNBiologicalStreamSampling/Shared%20Documents/General/Annual-Data-Packages/2022/NCRN_MBSS/NCRN_MBSS_2022.mdb?csf=1&web=1&e=hJHp4F
+    - ~OneDrive - DOI\Documents - NPS-NCRN-Biological Stream Sampling\General\Annual-Data-Packages\2022\NCRN_MBSS
+    - Note: all other data source files are included in the repo
+3. Run `renv::autoload()` in your R session to pull project dependencies
+4. Run main.R in your R session
---
+++ main.R
@@ -0,0 +1,27 @@
+# this script is a workflow to build one EDD xlsx from all NCRN and collaborator Biological Stream Survey (BSS) source files
+
+# this script addresses https://github.com/NCRN/NCRN_DM/issues/56
+# contents of `data/` is from: https://doimspp.sharepoint.com/sites/NCRNBiologicalStreamSampling/Shared%20Documents/Forms/AllItems.aspx?csf=1&web=1&e=oKeaBg&cid=46a9b284%2D304d%2D4b63%2D9d30%2D64b97cdd0314&RootFolder=%2Fsites%2FNCRNBiologicalStreamSampling%2FShared%20Documents%2FGeneral%2FAnnual%2DData%2DPackages%2F2022%2FExamples&FolderCTID=0x0120002FA57F4A20C6EC439BFEBEF4FA9D88E0
+# database is from: https://doimspp.sharepoint.com/:u:/r/sites/NCRNBiologicalStreamSampling/Shared%20Documents/General/Annual-Data-Packages/2022/NCRN_MBSS/NCRN_MBSS_be_2022.mdb?csf=1&web=1&e=jjeJIg
+
+#----- load external libraries
+library(data.table)
+library(tidyverse)
+library(dplyr)
+library(readxl)
+library(RODBC)
+library(openxlsx)
+
+#----- prep
+rm(list=ls())
+RODBC::odbcCloseAll() # close any open db connections
+
+#----- load project functions
+source("scripts/edd/buildEDD.R")
+
+#----- specify NCRN db
+# db <- "C:/Users/cwainright/OneDrive - DOI/Documents - NPS-NCRN-Biological Stream Sampling/General/Annual-Data-Packages/2022/NCRN_MBSS/NCRN_MBSS_be_2022.mdb"# "old" database https://doimspp.sharepoint.com/:u:/r/sites/NCRNBiologicalStreamSampling/Shared%20Documents/General/Annual-Data-Packages/2022/NCRN_MBSS/NCRN_MBSS_be_2022.mdb?csf=1&web=1&e=jjeJIg
+# db <- "data/NCRN_MBSS_be_2022.mdb" # "new" database after Jones added missing locations # \OneDrive - DOI\Documents - NPS-NCRN-Biological Stream Sampling\General\Annual-Data-Packages\2022\NCRN_MBSS\NCRN_MBSS_be_2022.mdb
+
+#----- execute script
+make_edd(write = TRUE, db = "data/ncrn/NCRN_MBSS_be_2022.mdb")
