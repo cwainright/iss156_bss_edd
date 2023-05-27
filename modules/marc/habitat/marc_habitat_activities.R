e90de151767fbf8d6cb720ccbc33abae39978957
cwainright-nps <charles_wainright@nps.gov>
2023-05-27 12:13:48 -0400

    made `Assemblage_Sampled_Name` consistent among data sources

--- modules/marc/habitat/marc_habitat_activities.R
+++ modules/marc/habitat/marc_habitat_activities.R
@@ -63,7 +63,7 @@ marc_habitat_activities <- function(habitat_marc2022, habitat_marc2021, example,
             real[5] <- "Field Msr/Obs" # "Activity_Type"; choices are: 1) 'Field Msr/Obs' and 2) 'Sample-Routine'
             real[6] <- "Water" # "Medium"  choices are "Water", "Air", and "Other" in `example`
             real[7] <- NA # "Medium_Subdivision"
-            real[8] <- "Abiotic stream habitat" # "Assemblage_Sampled_Name"
+            real[8] <- "Stream benthic habitat inventory" # "Assemblage_Sampled_Name"
             real[9] <- format(as.Date(df$Start_Date), "%Y-%m-%d") # "Activity_Start_Date"
             real[10] <- NA # "Activity_Start_Time" 
             real[11] <- "Eastern Time - Washington, DC" # "Activity_Start_Time_Zone" 
--- modules/ncrn/habitat/ncrn_habitat_activities.R
+++ modules/ncrn/habitat/ncrn_habitat_activities.R
@@ -44,7 +44,7 @@ ncrn_habitat_activities <- function(results_list, example){
             real[5] <- "Field Msr/Obs" # "Activity_Type"; choices are: 1) 'Field Msr/Obs' and 2) 'Sample-Routine'
             real[6] <- "Water" # "Medium"  choices are "Water", "Air", and "Other" in `example`
             real[7] <- NA # "Medium_Subdivision"
-            real[8] <- "Stream habitat inventory" # "Assemblage_Sampled_Name"
+            real[8] <- "Stream benthic habitat inventory" # "Assemblage_Sampled_Name"
             real[9] <- format(df$Start_Date, "%Y-%m-%d") # "Activity_Start_Date"
             real[10] <- format(df$Start_Time, "%H:%M") # "Activity_Start_Time" 
             real[11] <- "Eastern Time - Washington, DC" # "Activity_Start_Time_Zone" 
